c1d1ed634ef072b8201c69eeef52212935cf25c9
Maxim Pekar <maximpekar@mail.ru>
2026-05-06 18:39:04 +0300

    Main form - начало

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,66 @@
 import '../scss/styles.scss'
 
 // Import only the Bootstrap components we need
-import { Popover, Button } from 'bootstrap';
+// import { Modal } from 'bootstrap';
+// const  myMod = new Modal(document.getElementById('staticBackdrop'), {});
+const btnSearch = document.getElementById('btnSearch');
+const divResult = document.getElementById('divResult');
+const bookAuthor = document.getElementById('bookAuthor');
+const bookName = document.getElementById('bookName');
 
-// Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-	.forEach(popover => {
-		new Popover(popover)
-	});
+const res = [
+	{bookAuthor: 'Pushkin', bookName: 'Capitan\'s daughter', bookInfo: '', bookWorks: []},
+	{bookAuthor: 'Pushkin', bookName: 'General\'s daughter', bookInfo: '', bookWorks: []},
+	{bookAuthor: 'Pushkin', bookName: 'Lieutenant\'s daughter', bookInfo: '', bookWorks: []},
+	{bookAuthor: 'Pushkin', bookName: 'Marshall\'s daughter', bookInfo: '', bookWorks: []},
+	{bookAuthor: 'Pushkin', bookName: 'Soldat\'s daughter', bookInfo: '', bookWorks: []},
+	{bookAuthor: 'Pushkin', bookName: 'Officer\'s daughter', bookInfo: '', bookWorks: []},
+	{bookAuthor: 'Александр Сергеевич Пушкин (великий русский поэт)', bookName: 'История села Горюхина, рассказанная подпоручиком' +
+			' в отставке, писарем Нижегородского уезда Квакиным Петром Вениаминовичем',
+		bookInfo: 'Много всякого забавного', bookWorks: ['Jlkldslsklk lskdlkdsl','Jlkldslsklk lskdlkdsl','Jlkldslsklk lskdlkdsl',]},
+]
+
+btnSearch.onclick = async () => {
+	const data = await getData();
+	setData(divResult, data);
+}
+
+
+async function getData () {
+	const params = {Author: bookAuthor.value, BookName: bookName.value};
+	try {
+		const response = await fetch('http://pekarapi/book/book/list',{
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8'
+			},
+			body: JSON.stringify(params),
+		});
+		if (!response.ok) {
+			console.log(await response.text());
+			return [];
+		}
+		return await response.json();
+	} catch (error) {	                    // Если есть проблемы, сообщаем в консоль и возвращаем пустой массив
+		console.error("Ошибка получения данных: ", error);
+		return [];
+	}
+	// return res;
+}
+
+
+
+function setData(elem, data) {
+	elem.innerHTML = data.reduce(
+		(ac, cur) => ac +
+			'<details><summary>' +
+			`<span style="font-weight: bold">${cur.bookAuthor}</span> &nbsp;` +
+			`<span style="color: blue">${cur.bookName}</span></summary>` +
+			`<span style="font-style: italic">${cur.bookInfo}</span>` +
+			cur.bookWorks.reduce(
+				(a, c) => a + `<p class="mb-1">${c}`, ''
+			) +
+			'</details>',
+		'',
+	)
+}
