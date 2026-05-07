d34f2acc1374006cdd93475c2083686ff6a166a1
Maxim Pekar <maximpekar@mail.ru>
2026-05-07 18:10:10 +0300

    Main form, добавлено переключение поиска произведений, надо сделать под это api

--- src/js/main.js
+++ src/js/main.js
@@ -4,15 +4,28 @@ const btnSearch = document.getElementById('btnSearch');
 const divResult = document.getElementById('divResult');
 const bookAuthor = document.getElementById('bookAuthor');
 const bookName = document.getElementById('bookName');
+const elemFindType = document.getElementById('findType');
+let findType = true;
 
 btnSearch.onclick = async () => {
-	const data = await getData();
-	setData(divResult, data);
+	const data = await getData(findType);
+	setData(divResult, data, findType);
 }
 
 
-async function getData () {
-	const params = {Author: bookAuthor.value, BookName: bookName.value};
+elemFindType.onclick = () => {
+	findType = !findType;
+	if (findType) {
+		elemFindType.innerHTML = 'Поиск книг';
+	}
+	else {
+		elemFindType.innerHTML = '<span class="text-primary">Поиск произведений</span>';
+	}
+}
+
+
+async function getData (findType = true) {
+	const params = {Author: bookAuthor.value, BookName: bookName.value, FindType: findType};
 	try {
 		const response = await fetch('https://api.pekarlib.ru/book/book/B1p1s9Z4b1V5XyIi',{
 			method: "POST",
@@ -33,17 +46,22 @@ async function getData () {
 }
 
 
-function setData(elem, data) {
-	elem.innerHTML = data.reduce(
-		(ac, cur) => ac +
-			'<details><summary>' +
-			`<span class="fw-bold">${cur.bookAuthor}</span> &nbsp;` +
-			`<span class="text-success">${cur.bookName}</span></summary>` +
-			`<span class="fst-italic">${cur.bookInfo}</span>` +
-			cur.bookWorks.reduce(
-				(a, c) => a + `<p class="mb-1 text-secondary">${c}`, ''
-			) +
-			'</details>',
-		'',
-	)
+function setData(elem, data, findType = true) {
+	if (findType) {
+		elem.innerHTML = data.reduce(
+			(ac, cur) => ac +
+				'<details><summary>' +
+				`<span class="fw-bold">${cur.bookAuthor}</span> &nbsp;` +
+				`<span class="text-success">${cur.bookName}</span></summary>` +
+				`<span class="fst-italic">${cur.bookInfo}</span>` +
+				cur.bookWorks.reduce(
+					(a, c) => a + `<p class="mb-1 text-secondary">${c}`, ''
+				) +
+				'</details>',
+			'',
+		);
+	}
+	else {
+		elem.innerHTML = '<span class="text-danger">Ничего не найдено!</span>';
+	}
 }
