9bbf36a42014007891bf393d611ee93b599c26d7
Maxim Pekar <maximpekar@mail.ru>
2026-05-07 17:44:18 +0300

    Main form - 1.8

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,13 @@
-// Import our custom CSS
 import '../scss/styles.scss'
 
-// Import only the Bootstrap components we need
-// import { Modal } from 'bootstrap';
-// const  myMod = new Modal(document.getElementById('staticBackdrop'), {});
 const btnSearch = document.getElementById('btnSearch');
 const divResult = document.getElementById('divResult');
 const bookAuthor = document.getElementById('bookAuthor');
 const bookName = document.getElementById('bookName');
 
-const res = [
-	{bookAuthor: 'Pushkin', bookName: 'Capitan\'s daughter', bookInfo: '', bookWorks: []},
-	{bookAuthor: 'Pushkin', bookName: 'General\'s daughter', bookInfo: '', bookWorks: []},
-	{bookAuthor: 'Pushkin', bookName: 'Lieutenant\'s daughter', bookInfo: '', bookWorks: []},
-	{bookAuthor: 'Pushkin', bookName: 'Marshall\'s daughter', bookInfo: '', bookWorks: []},
-	{bookAuthor: 'Pushkin', bookName: 'Soldat\'s daughter', bookInfo: '', bookWorks: []},
-	{bookAuthor: 'Pushkin', bookName: 'Officer\'s daughter', bookInfo: '', bookWorks: []},
-	{bookAuthor: 'Александр Сергеевич Пушкин (великий русский поэт)', bookName: 'История села Горюхина, рассказанная подпоручиком' +
-			' в отставке, писарем Нижегородского уезда Квакиным Петром Вениаминовичем',
-		bookInfo: 'Много всякого забавного', bookWorks: ['Jlkldslsklk lskdlkdsl','Jlkldslsklk lskdlkdsl','Jlkldslsklk lskdlkdsl',]},
-]
-
 btnSearch.onclick = async () => {
 	const data = await getData();
 	setData(divResult, data);
-	// console.log(data);
 }
 
 
@@ -47,9 +30,7 @@ async function getData () {
 		console.error("Ошибка получения данных: ", error);
 		return [];
 	}
-	// return res;
 }
-
 
 
 function setData(elem, data) {
@@ -60,7 +41,7 @@ function setData(elem, data) {
 			`<span class="text-success">${cur.bookName}</span></summary>` +
 			`<span class="fst-italic">${cur.bookInfo}</span>` +
 			cur.bookWorks.reduce(
-				(a, c) => a + `<p class="mb-1 text-black-50">${c}`, ''
+				(a, c) => a + `<p class="mb-1 text-secondary">${c}`, ''
 			) +
 			'</details>',
 		'',
